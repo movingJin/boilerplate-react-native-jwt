bb7ea94008fa69ee122bb1d5fb45bebd23a551eb
movingJin <shdlehdwls@gmail.com>
2024-02-03 22:49:04 +0900

    feat: tab 메뉴, icon 추가  - MaterialIcons import

--- src/Tab.js
+++ src/Tab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import Map from './Map'
 import News from './News'
 import GptNews from './GptNews'
@@ -9,9 +10,33 @@ const Tab = createBottomTabNavigator();
 const TabNavigation = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name='Map' component={Map} />
-            <Tab.Screen name='News' component={News} />
-            <Tab.Screen name='GptNews' component={GptNews} />
+            <Tab.Screen
+                name='Map'
+                component={Map}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Icon name="map" color={color} size={size} />
+                      )
+                }}
+                />
+            <Tab.Screen
+                name='News'
+                component={News}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Icon name="newspaper" color={color} size={size} />
+                      )
+                }}
+                />
+            <Tab.Screen
+                name='GptNews'
+                component={GptNews}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Icon name="article" color={color} size={size} />
+                      )
+                }}
+                />
             {/* <Tab.Screen name='Settings' component={Settings} /> */}
         </Tab.Navigator>
     );
