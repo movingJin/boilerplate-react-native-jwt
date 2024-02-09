83836713c172cc4ce9330e7f55fcd9fe6cf380b1
movingJin <shdlehdwls@gmail.com>
2024-02-09 23:23:18 +0900

    feat: modal popup 리팩토링  - passing argument 기능 추가

--- src/News.js
+++ src/News.js
@@ -3,23 +3,36 @@ import {View, Text, StyleSheet, TouchableOpacity, Modal, Image} from 'react-nati
 //import {TouchableOpacity} from 'react-native-gesture-handler';
 import { FlashList } from "@shopify/flash-list";
 
+class Item {
+  constructor({title, body, issueDate, publisher, author, img}){
+    this.title = title;
+    this.body = body;
+    this.issueDate = issueDate;
+    this.publisher = publisher;
+    this.author = author;
+    this.img = img;
+  }
+}
+
 export default class News extends Component {
 
   constructor(){
     super();
     this.state={
       datas: [
-        {title: "제목1", body:"본문1", issue_date: '2024-02-04', publisher: '동아일보', author:'이동진', img:'require(그림경로)'},
-        {title: "제목2", body:"본문2", issue_date: '2024-02-04', publisher: '조선일보', author:'박지연', img:'require(그림경로)'}
+        {title: "제목1", body:"본문1", issueDate: '2024-02-04', publisher: '동아일보', author:'이동진', img:'require(그림경로)'},
+        {title: "제목2", body:"본문2", issueDate: '2024-02-04', publisher: '조선일보', author:'박지연', img:'require(그림경로)'}
       ],
-      isModalVisible: false
+      isModalVisible: false,
+      selectedItem: null
     };
   }
 
-  toggleModal = () => {
+  toggleModal = (item=null) => {
     this.setState({
       isModalVisible: !this.state.isModalVisible
     });
+    this.setState({selectedItem: item});
   };
 
   render() {
@@ -31,36 +44,21 @@ export default class News extends Component {
           renderItem={this.renderItem}
           estimatedItemSize={200}
         />
-        {/* {this.state.isModalVisible ? this.popupItem() : null} */}
-        {this.state.isModalVisible &&
-          <Modal
-            visible={this.state.isModalVisible}
-            transparent={true}
-            animationType='slide'
-            onRequestClose={this.toggleModal}
-          >
-            <View style={style.modelStyle}>
-              <View style={style.modelWrapperStyle}>
-                <Text style={style.itemTitle}>제목</Text>
-                <Text style={style.itemBody}>본문</Text>
-              </View>
-            </View>
-          </Modal>
-        }
+        {this.state.isModalVisible && this.popupItem()}
       </View>
     );
   }
 
   renderItem=({item})=>{
     return(
-      <TouchableOpacity style={style.itemView} onPress={this.toggleModal}>
+      <TouchableOpacity style={style.itemView} onPress={() => this.toggleModal(item)}>
           {/* <Image source={item.img} style={style.itemImg}></Image> */}
           <View style={{flexDirection:'column'}}>
               <Text style={style.itemTitle}>{item.title}</Text>
               <Text style={style.itemBody}>{item.body}</Text>
               <View style={{flexDirection:'row'}}>
                 <Text style={style.itemPublisher}>{item.publisher}</Text>
-                <Text style={style.itemIssueDate}>{item.issue_date}</Text>
+                <Text style={style.itemIssueDate}>{item.issueDate}</Text>
               </View>
           </View>
           
@@ -68,20 +66,37 @@ export default class News extends Component {
     );
   }
 
-  popupItem=(item)=>{
-    //console.log(item);
-    console.log("popupItem");
-    <Modal
+  popupItem=()=>{
+    return(
+    // <Modal
+    //   visible={this.state.isModalVisible}
+    //   transparent={true}
+    //   animationType='slide'
+    //   onRequestClose={this.toggleModal}
+    // >
+    //   <View style={{flexDirection:'column'}}>
+    //     <Text style={style.itemTitle}>{this.state.selectedItem.title}</Text>
+    //     <Text style={style.itemBody}>{this.state.selectedItem.title}</Text>
+    //   </View>
+    // </Modal>
+      <Modal
       visible={this.state.isModalVisible}
       transparent={true}
       animationType='slide'
-      onRequestClose={this.toggleModal}
-    >
-      <View style={{flexDirection:'column'}}>
-        <Text style={style.itemTitle}>{item.title}</Text>
-        <Text style={style.itemBody}>{item.body}</Text>
-      </View>
-    </Modal>
+      onRequestClose={() => this.toggleModal(null)}
+      >
+        <View style={style.modelStyle}>
+          <View style={style.modelWrapperStyle}>
+            <Text style={style.itemTitle}>{this.state.selectedItem.title}</Text>
+            <Text style={style.itemBody}>{this.state.selectedItem.body}</Text>
+            <View style={{flexDirection:'row'}}>
+              <Text style={style.itemPublisher}>{this.state.selectedItem.publisher}</Text>
+              <Text style={style.itemIssueDate}>{this.state.selectedItem.issueDate}</Text>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    )
   }
 }
 
