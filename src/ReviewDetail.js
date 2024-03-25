7d4d44b01552b43d6edb65fb96537899fa24226c
movingJin <shdlehdwls@gmail.com>
2024-03-25 20:53:56 +0900

    fix: deatil에 보증금 등 렌더링 항목추가

--- src/ReviewDetail.js
+++ src/ReviewDetail.js
@@ -22,8 +22,8 @@ export default class ReviewDetail extends Component {
       <View style={style.root}>
         <Text style={style.titleText}>{this.props.review.title}</Text>
         <Text >상세주소: {this.props.review.address}</Text>
-        <Text>보증금반환지연: {this.props.review.isReturnDelayed}</Text>
-        <Text>보증금: {this.props.review.deposit} (만원)</Text>
+        <Text>보증금반환지연: {this.props.review.isReturnDelayed ? "지연됨": "정상"}</Text>
+        <Text>보증금: {(this.props.review.deposit/10000)} (만원)</Text>
         <Text>거주기간: {this.props.review.fromDate} ~ {this.props.review.toDate === "9999-12-31" ? "거주중": this.props.review.toDate}</Text>
         <Text>계약일: {this.props.review.contractDate}</Text>
         <Text style={style.rating}>평점: {this.props.review.rating}</Text>
