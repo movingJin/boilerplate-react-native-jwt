737513b8348c09cfa32a47a99b60b4426141326a
movingJin <shdlehdwls@gmail.com>
2024-08-20 21:56:22 +0900

    fix: web app  - vector-icons 깨지는 현상 개선

--- index.web.js
+++ index.web.js
@@ -3,6 +3,24 @@ import App from './App.web'
 import { name as appName } from './app.json';
 import { Platform } from 'react-native';
 
+import MaterialIcons from 'react-native-vector-icons/Fonts/MaterialIcons.ttf';
+
+const MaterialIconsStyles = `@font-face {
+  src: url(${MaterialIcons});
+  font-family: MaterialIcons;
+}`;
+
+const style = document.createElement('style');
+style.type = 'text/css';
+
+if (style.styleSheet) {
+  style.styleSheet.cssText = MaterialIconsStyles;
+} else {
+  style.appendChild(document.createTextNode(MaterialIconsStyles));
+}
+
+document.head.appendChild(style);
+
 AppRegistry.registerComponent(appName, () => App);
 if (Platform.OS === 'web') {
     AppRegistry.runApplication(appName, {
